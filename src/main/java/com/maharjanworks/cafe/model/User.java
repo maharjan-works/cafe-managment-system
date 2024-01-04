8a0ed697d2f618a2a411b48be8eb85f5b6e418ba
maharjan.works <maharjan.works@gmail.com>
2024-01-04 10:43:19 -0500

    pilot project + dd configuration + user-model class added

---
+++ src/main/java/com/maharjanworks/cafe/model/User.java
@@ -0,0 +1,31 @@
+package com.maharjanworks.cafe.model;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "user_tbl")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String contactNumber;
+    private String email;
+    private String password;
+    private String status;
+    private String role;
+
+}
